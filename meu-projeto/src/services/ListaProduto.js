28a511e22e8e1a0e182589bf2b872221906ff546
PedroIgor <pedro.igor.pitl@gmail.com>
2026-06-24 01:08:19 -0300

    JWT na lista de produtos

--- meu-projeto/src/services/ListaProduto.js
+++ meu-projeto/src/services/ListaProduto.js
@@ -2,12 +2,23 @@ import axios from 'axios';
 
 export async function listaProdutos() {
     try {
-        const response = await axios.get("http://localhost:8080/produto/lista", {
-            headers: {
-                "Content-Type": "application/json",
+
+        const token = localStorage.getItem("token");
+
+        console.log("TOKEN PRODUTOS:", token);
+
+        const response = await axios.get(
+            "http://localhost:8080/produto/lista",
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                }
             }
-        });
+        );
+
         return response.data;
+
     } catch (error) {
         console.error("Erro na requisição:", error);
         throw error;
--- meu-projeto/src/services/alterarProdutoAdmin.js
+++ meu-projeto/src/services/alterarProdutoAdmin.js
@@ -1,37 +1,52 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function buscarProdutoPorId(idProduto) {
-    try {
-        // Usando template literal para interpolar a variável idProduto corretamente
-        const response = await axios.get(`http://localhost:8080/produto/${idProduto}`);
-        console.log("Produto buscado:", response.data);
-        
-        if (response.data) {
-            return response.data;  // Retorna os dados do produto
-        } else {
-            console.error("Produto não encontrado");
-            alert("Produto não encontrado.");
-            return null;
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await axios.get(
+      `http://localhost:8080/produto/${idProduto}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
         }
-    } catch (error) {
-        console.error("Erro ao buscar o produto:", error);
-        alert("Erro ao buscar o produto. Tente novamente.");
-        return null;
+      }
+    );
+
+    console.log("Produto buscado:", response.data);
+
+    if (response.data) {
+      return response.data;
     }
+
+    console.error("Produto não encontrado");
+    return null;
+
+  } catch (error) {
+    console.error("Erro ao buscar o produto:", error);
+    throw error;
+  }
 }
 
+export async function alterarProdutoAdmin(
+  idProduto,
+  formData,
+  selectedCategoriaId
+) {
+  const token = localStorage.getItem("token");
 
-export async function alterarProdutoAdmin(idProduto, formData, selectedCategoriaId) {
   const response = await axios.put(
     `http://localhost:8080/produto/atualizar/${idProduto}/${selectedCategoriaId}`,
     formData,
     {
       headers: {
-        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
       },
     }
   );
+
   console.log("Produto alterado:", response.data);
+
   return response.data;
 }
-
