8bab9dda00f0e56d6795800e8cc9fb47f359df94
PedroIgor <pedro.igor.pitl@gmail.com>
2026-06-24 01:17:25 -0300

    Feat: Liberação da pasta uploads para enviar as imagens ao front end

--- meu-projeto/src/services/NovoProdutoAdmin.js
+++ meu-projeto/src/services/NovoProdutoAdmin.js
@@ -1,17 +1,31 @@
 import axios from 'axios';
+
 export const cadastrarProduto = async (formData, selectedCategoriaId) => {
   try {
+
+    const token = localStorage.getItem("token");
+
+    console.log("TOKEN CADASTRAR PRODUTO:", token);
+
+    for (let pair of formData.entries()) {
+      console.log(pair[0], pair[1]);
+    }
+
     const response = await axios.post(
       `http://localhost:8080/produto/cadastrar/${selectedCategoriaId}`,
       formData,
       {
         headers: {
+          Authorization: `Bearer ${token}`,
           'Content-Type': 'multipart/form-data',
         },
       }
     );
+
     return response.data;
+
   } catch (error) {
+    console.error("Erro ao cadastrar produto:", error);
     throw error;
   }
 };
