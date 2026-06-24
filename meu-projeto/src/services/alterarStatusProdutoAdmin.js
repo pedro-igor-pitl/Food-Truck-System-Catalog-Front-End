0c67c488008d13a5caeb0bccdd85aebd8e643e3e
PedroIgor <pedro.igor.pitl@gmail.com>
2026-06-24 01:19:58 -0300

    Feat: JWT de alterar status de produto

--- meu-projeto/src/services/alterarStatusProdutoAdmin.js
+++ meu-projeto/src/services/alterarStatusProdutoAdmin.js
@@ -1,23 +1,31 @@
 export async function alterarStatusProdutoAdmin(id) {
   try {
-    const response = await fetch(`http://localhost:8080/produto/alternar-status/${id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-    });
-    
+
+    const token = localStorage.getItem("token");
+
+    const response = await fetch(
+      `http://localhost:8080/produto/alternar-status/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+      }
+    );
 
     if (response.status === 204) {
-      return { status: 204 }; 
+      return { status: 204 };
     }
 
-  
     if (!response.ok) {
       throw new Error(`Erro ao alterar produto (status ${response.status})`);
     }
 
     return await response.json();
+
   } catch (error) {
-    console.error('Erro ao alterar Produto:', error.message);
+    console.error("Erro ao alterar Produto:", error.message);
     throw error;
   }
 }
