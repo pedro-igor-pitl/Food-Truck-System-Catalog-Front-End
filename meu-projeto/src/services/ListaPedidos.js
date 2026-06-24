5f52c2824a6130340cd14b6ee3e0d65779402eff
PedroIgor <pedro.igor.pitl@gmail.com>
2026-06-24 02:37:39 -0300

    Feat: JWT na rota de pedido admin

--- meu-projeto/src/services/ListaPedidos.js
+++ meu-projeto/src/services/ListaPedidos.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function listaPedidos() {
 
-    const token = localStorage.getItem("adminToken");
+    const token = localStorage.getItem("token");
 
     const response = await axios.get(
         "http://localhost:8080/pedido/lista",
